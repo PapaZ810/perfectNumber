102968457af2fdeea7890e39074619a227278d2c
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-03-07 20:18:52 -0700

    use sympy
    bc i do not know how to make a good is prime function

--- compute.py
+++ compute.py
@@ -1,21 +1,12 @@
-import math
+from sympy import isprime
 
 
 def perfectNumber():
-    for n in range(1, 500):
+    for n in range(1, 5000):
         n1 = 2**n - 1
         n2 = 2**(n-1)
         if isprime(n1):
             print("Number", n2*n1, "is a perfect number")
-
-
-def isprime(n):
-    if n < 1:
-        return False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
 
 
 if __name__ == "__main__":
