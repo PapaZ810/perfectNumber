38c8c3a99c59b573b63dd689a3c36fc225dc538c
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-03-07 21:37:37 -0700

    fixed crash on big numba

--- compute.py
+++ compute.py
@@ -1,8 +1,11 @@
 from sympy import isprime
+import sys
+
+sys.set_int_max_str_digits(10000)
 
 
 def perfectNumber():
-    for n in range(1, 5000):
+    for n in range(1, 10000):
         n1 = 2**n - 1
         n2 = 2**(n-1)
         if isprime(n1):
